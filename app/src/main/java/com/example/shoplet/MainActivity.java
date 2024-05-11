4b5024d8fff644e45da852cf1165c33f1482af52
Usman Faisal <usmanfaisal49@gmail.com>
2024-05-11 23:09:49 +0500

    updated

--- app/src/main/java/com/example/shoplet/MainActivity.java
+++ app/src/main/java/com/example/shoplet/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.shoplet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        setButtonClickListener();
+    }
+
+    void init()
+    {
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+    }
+
+    void setButtonClickListener()
+    {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
