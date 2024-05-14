fd2606e8d6f27641915cab4f372d0bfec993847a
Nisa Nadeem <nisa.nadeem@dubizzlelabs.com>
2024-05-14 17:26:38 +0500

    homepage done

--- app/src/main/java/com/example/shoplet/FlashSaleItem.java
+++ app/src/main/java/com/example/shoplet/FlashSaleItem.java
@@ -1,4 +1,47 @@
 package com.example.shoplet;
 
 public class FlashSaleItem {
+    private int imageResource;
+    private String name;
+    private String rating;
+    private String price;
+
+    public FlashSaleItem(int imageResource, String name, String rating, String price) {
+        this.imageResource = imageResource;
+        this.name = name;
+        this.rating = rating;
+        this.price = price;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    public void setImageResource(int imageResource) {
+        this.imageResource = imageResource;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
